a808a56225e30e171192be885a7e5a7f659fc118
Mary <89971822+Mary-fox@users.noreply.github.com>
2023-03-22 12:27:56 +0300

    feat: add hover in header and footer

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -13,8 +13,9 @@ function Accordion(props) {
       <div className="accordion">
         <div className={`accordion__header ${isExpanded ? 'accordion__header_active' : ''}`} onClick={toggleAccordion}>
             <h3>{props.title}</h3>
-            <span>{isExpanded ? <svg width="32" height="22" viewBox="0 0 32 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 21L15.85 1L31 21" stroke="#2B2B2B"/></svg>
-                                : <svg width="32" height="22" viewBox="0 0 32 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M31 1L16.15 21L0.999999 1" stroke="#2B2B2B"/></svg>
+            <span>{isExpanded ? <svg width="32" height="17" viewBox="0 0 32 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 16L15.85 0.999999L31 16" stroke="#262626"/></svg>
+                                : <svg width="32" height="17" viewBox="0 0 32 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M31 1L16.15 16L0.999999 1" stroke="#262626"/></svg>
+                                
             }</span>
         </div>
         <div className={"accordion__content" + (isExpanded ? " accordion__content--expanded" : "")}>
